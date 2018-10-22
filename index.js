c2395bc88ee15448e1b024f804fe0f86c31bdbf1
Elijah Prince <elijahprince73@gmail.com>
2018-10-22 00:57:54 -0700

    getting ready for production

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+app.use(express.static('build'));
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
